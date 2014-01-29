a1fd4925cac89c80b182591b0ceb7109a6347d92
Jonathan El-Bizri <git@elbizri.com>
2014-01-28 18:51:54 -0800

    added login status toggle to main controller

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,20 @@
 'use strict';
 
 angular.module('fireUser').value('FireUserConfig',{
-    url:"https://fireuser.firebaseio.com",
+    url:"https://fireuser.firebaseio.com/",
     iconCss:'fontawesome'
     });
 
 angular.module('FireUserDemo', ['fireUser'])
   .controller('Main',function ($scope) {
-    $scope.loginstatus = "not logged in"
+
+    $scope.loginstatus = 'not logged in'
+
+    $scope.$on('fireuser:login',function () {
+      $scope.loginstatus = 'logged in'
+    })
+
+    $scope.$on('fireuser:logout',function () {
+      $scope.loginstatus = 'not logged in'
+    })
   })
